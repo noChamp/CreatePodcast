e04353c0558e01ab22c2302e0108e846519f0d68
noChamp <pelyem@gmail.com>
2018-01-01 10:30:44 +0200

    trigger downloading the episode every 5 minutes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+
+//the reasons I use express framework is to satiafy Heroku:
+//1. the app won't end, since express holds a thread that always listen....
+//2. when I open the app on Heroku site - express returns "Hello World!" so the app doesn't crush,
+//   since the app is considered 'web' app - so I need to supply a response to a hit to the app URL
+//   (somewhere I can configure Heroku to treat my app diferently in a procfile. see here https://devcenter.heroku.com/articles/getting-started-with-nodejs#define-a-procfile)
+
+const myApp = require('./index');
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log('app listening on port ' + port));
+
+//start my app
+setInterval(myApp.checkAndCreatePodcast, 300000); // every 5 min
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var atob = require('atob');
 var request = require('request');
 var fs = require('fs');
 
-var url = 'http://chirb.it/8HEdwm' //(invoke from zappier)
 
 /*
 
@@ -25,62 +24,71 @@ use bitly node package?
 
 */
 
-//read the html
-request.get(url, function (error, response, body)
+exports.checkAndCreatePodcast = CheckAndAddToPopdcast;
+
+function CheckAndAddToPopdcast()
 {
-    if (!error && response.statusCode == 200)
+    console.log("checking for new episode");
+    
+    var url = 'http://chirb.it/8HEdwm' //(invoke from zappier)
+
+    //read the html
+    request.get(url, function (error, response, body)
     {
-        //give episode name from <div class="chirbit-title">
-        var regexp = new RegExp('<div class="chirbit-title">(.*?)</div>');
-        var match_array = regexp.exec(body);
-        var name = match_array[1]; //the value of chirbit-title
-        console.log("chirbit-title: " + name);
-
-
-        // find 'data-fd' and copy its value
-        // i used https://regex101.com/ for help
-        var myRegexp = new RegExp('data-fd="(.*?)"');
-        var match = myRegexp.exec(body);
-        var data_fd = match[1]; //the value of data-fd
-        console.log("data-fd: " + data_fd);
-
-        //decrypt the real url
-        var soundURL = atob(reverse(data_fd));
-
-        console.log("url: " + soundURL);
-
-        function reverse(s)
+        if (!error && response.statusCode == 200)
         {
-            return s.split('').reverse().join('');
+            //give episode name from <div class="chirbit-title">
+            var regexp = new RegExp('<div class="chirbit-title">(.*?)</div>');
+            var match_array = regexp.exec(body);
+            var name = match_array[1]; //the value of chirbit-title
+            console.log("chirbit-title: " + name);
+
+
+            // find 'data-fd' and copy its value
+            // i used https://regex101.com/ for help
+            var myRegexp = new RegExp('data-fd="(.*?)"');
+            var match = myRegexp.exec(body);
+            var data_fd = match[1]; //the value of data-fd
+            console.log("data-fd: " + data_fd);
+
+            //decrypt the real url
+            var soundURL = atob(reverse(data_fd));
+
+            console.log("url: " + soundURL);
+
+            function reverse(s)
+            {
+                return s.split('').reverse().join('');
+            }
+
+            function errorInDownloadingTheFile(error)
+            {
+                //todo: when episode is expired - email me 'failed'. examine the fileStream. fileStream size = 0 in this case
+                console.log("failed to download the episode");
+            }
+
+            function errorInSavingFileToDisc(error)
+            {
+                //todo: email if failed to save to disc
+                console.log("failed to save episode to disc");
+            }
+
+            //download the episode
+            var fileStream =
+                request(soundURL)
+                .on('error', function(e){errorInDownloadingTheFile(e)})
+                .pipe(fs.createWriteStream(name+'.mp3'))
+                .on('error', function(e){errorInSavingFileToDisc(e)});
+            fileStream.on('finish', function ()
+            {
+                //todo: email me the success
+                console.log("done!");
+            });
         }
-
-        function errorInDownloadingTheFile(error)
+        else
         {
-            //todo: when episode is expired - email me 'failed'. examine the fileStream. fileStream size = 0 in this case
-            console.log("failed to download the episode");
+            //signal zapier to send me an email with error or response code
+            console.log("error: " + error + ". statusCode: " + response.statusCode);
         }
-
-        function errorInSavingFileToDisc(error)
-        {
-            //todo: email if failed to save to disc
-            console.log("failed to save episode to disc");
-        }
-
-        //download the episode
-        var fileStream =
-            request(soundURL)
-            .on('error', function(e){errorInDownloadingTheFile(e)})
-            .pipe(fs.createWriteStream(name+'.mp3'))
-            .on('error', function(e){errorInSavingFileToDisc(e)});
-        fileStream.on('finish', function ()
-        {
-            //todo: email me the success
-            console.log("done!");
-        });
-    }
-    else
-    {
-        //signal zapier to send me an email with error or response code
-        console.log("error: " + error + ". statusCode: " + response.statusCode);
-    }
-});
+    });
+}
