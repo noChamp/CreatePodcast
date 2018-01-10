109c0ea92e8d399e49925a2a63d75e3359ca19f6
noChamp <pelyem@gmail.com>
2018-01-10 15:42:43 +0200

    some order

--- index.js
+++ index.js
@@ -25,6 +25,62 @@ use bitly node package?
 
 */
 
+function getEpisodeName(body)
+{
+    //give episode name from <div class="chirbit-title">
+    var regexp = new RegExp('<div class="chirbit-title">(.*?)</div>');
+    var match_array = regexp.exec(body);
+    var name = match_array[1]; //the value of chirbit-title
+
+    console.log("chirbit-title: " + name);
+
+    return name;
+}
+
+function getEpisodeDataFd(body)
+{
+    // find 'data-fd' and copy its value
+    // i used https://regex101.com/ for help
+    var myRegexp = new RegExp('data-fd="(.*?)"');
+    var match = myRegexp.exec(body);
+    var data_fd = match[1]; //the value of data-fd
+
+    console.log("data-fd: " + data_fd);
+
+    return data_fd;
+}
+
+function reverse(s)
+{
+    return s.split('').reverse().join('');
+}
+
+function errorInDownloadingTheFile(error)
+{
+    //todo: when episode is expired - email me 'failed'. examine the fileStream. fileStream size = 0 in this case
+    console.log("failed to download the episode");
+}
+
+function errorInSavingFileToDisc(error)
+{
+    //todo: email if failed to save to disc
+    console.log("failed to save episode to disc");
+}
+
+function downloadEpisode(soundURL, name)
+{
+    var fileStream = request(soundURL)
+                .on('error', function(e){errorInDownloadingTheFile(e)})
+                .pipe(fs.createWriteStream(name+'.mp3'))
+                .on('error', function(e){errorInSavingFileToDisc(e)});
+
+    fileStream.on('finish', function ()
+    {
+        //todo: email me the success
+        console.log("done!");
+    });
+}
+
 exports.checkAndCreatePodcast = CheckAndAddToPopdcast;
 
 function CheckAndAddToPopdcast()
@@ -38,53 +94,15 @@ function CheckAndAddToPopdcast()
     {
         if (!error && response.statusCode == 200)
         {
-            //give episode name from <div class="chirbit-title">
-            var regexp = new RegExp('<div class="chirbit-title">(.*?)</div>');
-            var match_array = regexp.exec(body);
-            var name = match_array[1]; //the value of chirbit-title
-            console.log("chirbit-title: " + name);
-
-
-            // find 'data-fd' and copy its value
-            // i used https://regex101.com/ for help
-            var myRegexp = new RegExp('data-fd="(.*?)"');
-            var match = myRegexp.exec(body);
-            var data_fd = match[1]; //the value of data-fd
-            console.log("data-fd: " + data_fd);
+            name = getEpisodeName(body);
+            data_fd = getEpisodeDataFd(body);
 
             //decrypt the real url
             var soundURL = atob(reverse(data_fd));
 
             console.log("url: " + soundURL);
 
-            function reverse(s)
-            {
-                return s.split('').reverse().join('');
-            }
-
-            function errorInDownloadingTheFile(error)
-            {
-                //todo: when episode is expired - email me 'failed'. examine the fileStream. fileStream size = 0 in this case
-                console.log("failed to download the episode");
-            }
-
-            function errorInSavingFileToDisc(error)
-            {
-                //todo: email if failed to save to disc
-                console.log("failed to save episode to disc");
-            }
-
-            //download the episode
-            var fileStream =
-                request(soundURL)
-                .on('error', function(e){errorInDownloadingTheFile(e)})
-                .pipe(fs.createWriteStream(name+'.mp3'))
-                .on('error', function(e){errorInSavingFileToDisc(e)});
-            fileStream.on('finish', function ()
-            {
-                //todo: email me the success
-                console.log("done!");
-            });
+            downloadEpisode(soundURL, name);
         }
         else
         {
